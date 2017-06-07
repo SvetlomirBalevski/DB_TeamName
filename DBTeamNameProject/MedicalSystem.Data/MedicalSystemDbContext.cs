e71c8ae787323ce4511f284b6fe636fd29d1ff21
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2017-06-07 14:44:25 +0300

    Added MedicalSystemDbContext.cs

---
+++ DBTeamNameProject/MedicalSystem.Data/MedicalSystemDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MedicalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalSystem.Data
+{
+    public class MedicalSystemDbContext : DbContext
+    {
+        public MedicalSystemDbContext()
+            : base("MedicalSystemDb")
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Specialty> Specialty { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Disease> Diseases { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+    }
+}
