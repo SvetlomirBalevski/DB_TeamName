4959c22c29c3a59071e789b813013843e8b5538c
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2017-06-07 13:40:54 +0300

    added project for medical models; added Hospital, Specialty and Doctor models

---
+++ DBTeamNameProject/MedicalSystem.Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalSystem.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual Hospital Hospital { get; set; }
+
+        public virtual Specialty Specialty { get; set; }
+    }
+}
---
+++ DBTeamNameProject/MedicalSystem.Models/Hospital.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalSystem.Models
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Doctor> Doctors { get; set; }
+    }
+}
